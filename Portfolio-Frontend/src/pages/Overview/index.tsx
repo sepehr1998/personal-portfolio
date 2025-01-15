8b0b3de820f295afe15f72b57ddf9a0905d7ee02
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-15 19:52:41 +0200

    social buttons created

--- Portfolio-Frontend/src/pages/Overview/index.tsx
+++ Portfolio-Frontend/src/pages/Overview/index.tsx
@@ -3,6 +3,7 @@ import developerImage from '../../assets/images/development.png';
 import './overview.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faComputer, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {faGithub, faLinkedin} from "@fortawesome/free-brands-svg-icons";
 
 function Overview() {
     const techStacks = ['Vue', 'React', 'Angular']
@@ -22,20 +23,33 @@ function Overview() {
                         />
                         <p className="my-0 mx-1">Developer</p>
                     </div>
-                    <br />
+                    <br/>
                     <p>Experienced frontend developer with +8 years of experience</p>
                     <div className="d-flex flex-row align-items-center mt-4">
                         <button className="glow-button me-4">
-                            <FontAwesomeIcon icon={ faComputer} className="me-3"/>Projects
+                            <FontAwesomeIcon icon={faComputer} className="me-3"/>Projects
                         </button>
                         <button className="glow-button">
-                            <FontAwesomeIcon icon={ faEnvelope} className="me-3"/>Contact
+                            <FontAwesomeIcon icon={faEnvelope} className="me-3"/>Contact
+                        </button>
+                    </div>
+
+                    <div className="d-flex flex-row align-items-center mt-5">
+                        <button
+                            className="glow-button me-4 bg-secondary bg-opacity-25"
+                            onClick={() => window.open('https://www.linkedin.com/in/sepehr-samadi/', '_blank')}>
+                            <FontAwesomeIcon icon={faLinkedin} className="text-light"/>
+                        </button>
+                        <button
+                            className="glow-button bg-secondary bg-opacity-25"
+                            onClick={() => window.open('https://www.github.com/sepehr1998/', '_blank')}>
+                            <FontAwesomeIcon icon={faGithub}/>
                         </button>
                     </div>
                 </div>
 
                 <div className="col-12 col-md-6 text-center text-md-end">
-                    <img src={developerImage} alt="web development" className="img-fluid" />
+                    <img src={developerImage} alt="web development" className="img-fluid"/>
                 </div>
             </div>
         </div>
