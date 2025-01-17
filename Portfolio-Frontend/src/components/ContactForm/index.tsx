675e30353667bf3dff6e906c4ee724f37e3f4d79
Sepehr Samadi <samadisepehr@gmail.com>
2025-01-17 18:33:09 +0200

    more styling for contact form [1]: background color for input fields changed [2]: name field added as a new field [3]: floating style for form labels

--- Portfolio-Frontend/src/components/ContactForm/index.tsx
+++ Portfolio-Frontend/src/components/ContactForm/index.tsx
@@ -1,14 +1,18 @@
-import {faEnvelope, faHeading, faPen} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import './contact-form.css'
-
 function ContactForm() {
     return(
         <>
             <h1 className="text-gradient">Get in Touch</h1>
             <form className="container mt-5">
                 <div className="form-floating mb-4 input-with-icon">
-                    <FontAwesomeIcon icon={faHeading} className="input-icon"/>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="title"
+                        placeholder="Title"
+                    />
+                    <label htmlFor="title">Name</label>
+                </div>
+                <div className="form-floating mb-4 input-with-icon">
                     <input
                         type="text"
                         className="form-control"
@@ -19,7 +23,6 @@ function ContactForm() {
                 </div>
 
                 <div className="form-floating mb-4 input-with-icon">
-                    <FontAwesomeIcon icon={faEnvelope} className="input-icon"/>
                     <input
                         type="email"
                         className="form-control"
@@ -30,7 +33,6 @@ function ContactForm() {
                 </div>
 
                 <div className="form-floating mb-4 input-with-icon">
-                    <FontAwesomeIcon icon={faPen} className="input-icon"/>
                     <textarea
                         className="form-control"
                         id="message"
@@ -40,7 +42,7 @@ function ContactForm() {
                     <label htmlFor="message">Message</label>
                 </div>
 
-                <button type="submit" className="btn btn-primary w-100">
+                <button type="submit" className="btn button-gradient w-50 text-light">
                     Submit
                 </button>
             </form>
